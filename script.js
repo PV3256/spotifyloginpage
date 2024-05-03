e7a34b383baab153be50d92776e4f93a699a85d7
PV33-PIXEL <Petladiip@gmail.com>
2024-05-03 16:37:39 +0530

    spotify

---
+++ script.js
@@ -0,0 +1,10 @@
+setTimeout(() => {
+    console.log("this is first line")
+}, 1000);
+console.log("this is second line")
+console.log("this is 3 line")
+console.log("this is 4 line")
+console.log("this is 5 line")
+function test(){
+    console.log("this is first line")
+}
